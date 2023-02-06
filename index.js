48c2838340616e605239fed073d49396be784f6d
Angel Stoyanov <angel@skyfi.com>
2023-02-06 13:18:44 +0200

    feat: preparation for new version

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log('hello world');
 console.log('Hello from the other side!');
 console.log('Introduce branching.');
 console.log('feat: Introduce branching.');
+console.log('feat: preparation for new version');
