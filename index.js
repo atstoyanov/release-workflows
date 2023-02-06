405272da3eb79336d2989b53c7269469f26a2da3
Angel Stoyanov <angel@skyfi.com>
2023-02-06 13:57:03 +0200

    fix: Demo

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log('Hello from the other side!');
 console.log('Introduce branching.');
 console.log('feat: Introduce branching.');
 console.log('feat: preparation for new version');
+console.log('fix: Demo');
